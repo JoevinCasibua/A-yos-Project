3b4e6f9137ee9a22516c496d9ea00c662c6e5f38
JoevinCasibua <jayjaycasibua555@gmail.com>
2026-07-22 10:19:27 +0800

    chore: remove unused root files and folders outside newuserfrontend

--- app/(worker)/reviews.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { ReviewsTab } from '@/components/ReviewsTab';
-import { workerReviews } from '@/constants/workerMockData';
-
-export default function WorkerReviewsScreen() {
-  return <ReviewsTab title="My Reviews" reviews={workerReviews} />;
-}
